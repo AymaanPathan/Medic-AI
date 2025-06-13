128d5158979fc1d43fc4ebd7415b9c954495cfea
aymaan <aymaanpathan5@gmail.com>
2025-06-13 23:27:57 +0530

    add client

---
+++ Client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div className="text-red-500 bg-gray-100 p-4">
+      <h1>Welcome to the Client App</h1>
+      <p>This is a simple React application.</p>
+    </div>
+  );
+};
+
+export default App;
