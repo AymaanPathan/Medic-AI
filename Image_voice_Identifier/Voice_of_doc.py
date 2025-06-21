867b7918bc28402c86699c439279775e58df3f81
aymaan <aymaanpathan5@gmail.com>
2025-06-21 23:31:48 +0530

    complete flow of image upload and voice output

--- Image_voice_Identifier/Voice_of_doc.py
+++ Image_voice_Identifier/Voice_of_doc.py
@@ -1,30 +1,66 @@
 import os
 import elevenlabs
 from elevenlabs.client import ElevenLabs
-
+import subprocess
+import platform
 from gtts import gTTS
-def text_to_speech_gtts(input_text,output_filePath):
-    langauge="en"
-    audioObj = gTTS(
-        text=input_text,
-        lang=langauge,
-        slow=False
-    )
-    audioObj.save(output_filePath)
+from pydub import AudioSegment
 
-input_text="Whatsapp"
-text_to_speech_gtts(input_text=input_text,output_filePath="gtts_testing.mp3")
+
+def autoPlay(mp3_path,wav_path):
+    try:
+        # Convert mp3 to wav
+        sound = AudioSegment.from_mp3(mp3_path)
+        sound.export(wav_path, format="wav")
+        os_name = platform.system()
+        if os_name == "Darwin":  # macOS
+            subprocess.run(['afplay', wav_path])
+        elif os_name == "Windows":
+            subprocess.run(['powershell', '-c', f'(New-Object Media.SoundPlayer "{wav_path}").PlaySync();'])
+        elif os_name == "Linux":
+            subprocess.run(['aplay', wav_path])
+        else:
+            raise OSError("Unsupported operating system")
+    except Exception as e:
+        print(f"An error occurred while trying to play the audio: {e}")
+
+
+
+def text_to_speech_gtts(input_text, mp3_path="gtts_testing.mp3", wav_path="gtts_testing.wav"):
+    language = "en"
+    # Save mp3
+    audioObj = gTTS(text=input_text, lang=language, slow=False)
+    audioObj.save(mp3_path)
+    try:
+        autoPlay(mp3_path=mp3_path,wav_path=wav_path)
+    except Exception as e:
+        print(f"An error occurred while trying to play the audio: {e}")
+
+# text_to_speech_gtts("autoplay testing is running")
+
+
 
 # 2 ELEVEN LABS
 ELEVENLABS_API_KEY=os.environ.get("ELEVENLABS_API_KEY")
-def text_to_speech_with_elevenlabs(input_text, output_filepath) :
-    client=ElevenLabs(api_key=ELEVENLABS_API_KEY)
-    audio=client. generate(
-    text= input_text,
-    voice="Jolly giant",
-    output_format= "mp3_22050_32",
-    model= "eleven_turbo_v2",
+def text_to_speech_with_elevenlabs(input_text, output_filepath, wav_path="gtts_testing.wav", voice_id=""):
+    client = ElevenLabs(api_key=ELEVENLABS_API_KEY)
+
+    audio = client.text_to_speech.convert(
+        voice_id=voice_id,
+        model_id="eleven_turbo_v2",
+        text=input_text,
+        output_format="mp3_22050_32"
     )
     elevenlabs.save(audio, output_filepath)
-text_to_speech_with_elevenlabs ( input_text, output_filepath="eleven habs_testing.mp3")
-#Step2: Use Model for Text output to Voice
+
+    try:
+        autoPlay(mp3_path=output_filepath, wav_path=wav_path)
+    except Exception as e:
+        print(f"An error occurred while trying to play the audio: {e}")
+
+text_to_speech_with_elevenlabs(
+    "You have cough and try to avoid icecream man and also dring hot soup" \
+    "it will help your throat",
+    output_filepath="eleven_labs_testing.mp3",
+    voice_id="TxGEqnHWrfWFTfGW9XjX"
+)
