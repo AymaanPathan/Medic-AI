14a489375f87c164fd3ea36dadbdfcad4c32bb45
aymaan <aymaanpathan5@gmail.com>
2025-06-25 14:07:26 +0530

    add ConversationBufferMemory

---
+++ Two_way_Chatting/Memory/ConverstaionBufferMemory.py
@@ -0,0 +1,12 @@
+from langchain.memory import ConversationBufferMemory
+from Two_way_Chatting.model_config import load_llm
+from langchain.chains import ConversationChain
+
+memory = ConversationBufferMemory(return_messages=True)
+
+
+chain = ConversationChain(llm=load_llm, memory=memory)
+
+response = chain.invoke({"input": "hi my name is aymaan"})
+response2 = chain.invoke({"input": "what is my name"})
+print(response2["response"])
